d7cf6240214bcf4811d7359cb6e74bb405581600
yerramasu <anily@hotmail.com>
2025-04-15 18:01:33 +0530

    Update auth-form-container.tsx

--- src/client/src/components/(auth)/auth-form-container.tsx
+++ src/client/src/components/(auth)/auth-form-container.tsx
@@ -202,28 +202,28 @@ export default function AuthFormContainer({
 			<div className="flex flex-col w-full max-w-sm mx-auto gap-12 z-10">
 				<div className="text-center">
 					<h1 className="text-4xl font-bold tracking-tight text-primary">
-						Welcome to OpenLIT
+						Welcome to IAgentic Admin Portal
 					</h1>
 					<p className="text-stone-600">
-						Open Source Platform for AI Engineering
+						Enterprise Platform for AI Engineering
 					</p>
 				</div>
 				{children}
 				<div className="grid grid-cols-2 text-center text-sm">
 					<Link
-						href={"https://github.com/openlit/openlit"}
+						href={"https://iagentic.ai"}
 						target="_blank"
 						className="w-full"
 					>
 						<Button
 							className={`w-full rounded-full gap-2 font-bold bg-stone-900 text-stone-50 hover:bg-stone-900/90 dark:bg-stone-900 dark:text-stone-50 dark:hover:bg-stone-900/90`}
 						>
-							Github
+							iAgentic
 							<GithubIcon className="ml-2 w-4" />
 						</Button>
 					</Link>
 					<Link
-						href="https://docs.openlit.io/latest/introduction"
+						href="https://iagentic.ai"
 						target="_blank"
 					>
 						<Button variant={"ghost"} className="hover:bg-stone-100 hover:text-stone-900 dark:hover:bg-stone-100 dark:hover:text-stone-900">
